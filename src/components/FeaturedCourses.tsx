6ad0bb81f43e70d88b344b0c0c609dc3a17fabf6
Harshitkant21 <harshitkant12@gmail.com>
2024-09-28 17:03:36 +0530

    update

---
+++ src/components/FeaturedCourses.tsx
@@ -0,0 +1,21 @@
+import courseData from "../data/music_courses.json";
+function FeaturedCourses() {
+  return (
+    <div className="py-12 bg-gray-900">
+      <div>
+        <div className="text-center">
+          <h2 className="text-base text-teal-500 font-semibold tracking-wide uppercase">
+            FEATURED COURSES
+          </h2>
+          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
+            Learn With The Best
+          </p>
+        </div>
+      </div>
+      <div>2</div>
+      <div className="mt-20 text-center"></div>
+    </div>
+  );
+}
+
+export default FeaturedCourses;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,43 @@
+"use client"; // when i want to use react hooks we have to write this line to make this file work for client side manupulation or DOM manupulation
+
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+
+function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
+        </Link>
+        <MenuItem setActive={setActive} active={active} item="Our Courses">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">All courses</HoveredLink>
+            <HoveredLink href="/">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/">Advance Composition</HoveredLink>
+            <HoveredLink href="/">Songwriting</HoveredLink>
+            <HoveredLink href="/">Music Produxtion</HoveredLink>
+          </div>
+        </MenuItem>
+        <Link href={"/contact"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contact Us"
+          ></MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  );
+}
+
+export default Navbar;
