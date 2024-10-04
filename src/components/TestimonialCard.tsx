6cb1b0cf933e3f666b6b35318dd861861ffb69de
Harshitkant21 <harshitkant12@gmail.com>
2024-10-04 21:01:53 +0530

    update

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -43,7 +43,7 @@ function MusicSchoolTestimonial() {
           <InfiniteMovingCards
             items={musicSchoolTestimonials}
             direction="right"
-            speed="slow"
+            speed="fast"
           />
         </div>
       </div>
